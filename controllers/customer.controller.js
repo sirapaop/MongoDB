471390704b687c7dc1ba187d357a2ba628ef2431
sirapaop <sirapaop2545@gmail.com>
2023-01-04 09:59:34 +0700

    export create

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -4,7 +4,7 @@ const customer = require('../models/customer.js')
 exports.index = (req, res) =>{
     res.send('<h1> Customer Application</h1><a href="/api/customer"> รายชื่อลูกค้า</a>')
 }
-
+//หา record ทั้งหมดใน DBมาแสดง 
 exports.findAll = (req, res) => {
     customer.find().then(data => {
         res.json(data)
@@ -13,4 +13,16 @@ exports.findAll = (req, res) => {
             msg: err.message
         })
     })
+
+exports.create = (req, res) => {
+    const c = new customer(req.body)
+
+    c.save().then(data =>{
+        res.json(data)
+    }).catch(res =>{
+        return res.status(500).json({
+            msg: "ไม่สามารถเพิ่มข้อมูลได้ เนื่องจาก :"+ err.message
+        })
+    })
+}
 }
