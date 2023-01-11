425b2ffbcbcb3ba01830a814ea7edf0e58b4b1cb
sirapaop <sirapaop2545@gmail.com>
2023-01-11 13:19:23 +0700

    herohu

--- index.js
+++ index.js
@@ -32,10 +32,14 @@ mongoose.connect(dbConfig.url)
 app.use(cors())
 require('./routes/customer.route.js')(app);
 
-const server = app.listen(3000, ()=>{
-    let port = server.address().port
-    console.log('Run at http://localhost:%s', port)
+const server = app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Run')
 })
+
+// const server = app.listen(3000, ()=>{
+//     let port = server.address().port
+//     console.log('Run at http://localhost:%s', port)
+// })
 
 function initCustomer(){
     let data = [
